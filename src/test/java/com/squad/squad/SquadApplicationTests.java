dcdca3b9b87534f3d92900ca4545c5db3d7f21f4
serkanursavas <serkan.ursavas@icloud.com>
2024-09-25 19:57:13 +0300

    feat: project setup (fixes #1)

---
+++ src/test/java/com/squad/squad/SquadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad.squad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
