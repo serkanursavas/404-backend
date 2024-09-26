b1dd8ee4a2fb73f8244b733a08cf651c83c0b0a4
serkanursavas <serkan.ursavas@icloud.com>
2024-09-26 16:48:06 +0300

    feat: add rating and update entities

--- src/main/java/com/squad/squad/entity/Roster.java
+++ src/main/java/com/squad/squad/entity/Roster.java
@@ -1,5 +1,7 @@
 package com.squad.squad.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Roster {
@@ -28,5 +31,56 @@ public class Roster {
     @ManyToOne
     @JoinColumn(name = "player_id", referencedColumnName = "id", nullable = false)
     private Player player;
+
+    @OneToMany(mappedBy = "roster")
+    private List<Rating> rate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTeamColor() {
+        return teamColor;
+    }
+
+    public void setTeamColor(String teamColor) {
+        this.teamColor = teamColor;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public List<Rating> getRate() {
+        return rate;
+    }
+
+    public void setRate(List<Rating> rate) {
+        this.rate = rate;
+    }
 
 }
