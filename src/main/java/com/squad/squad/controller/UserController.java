cb7940c78f9ed854640cd50e153babf704602c23
serkanursavas <serkan.ursavas@icloud.com>
2024-09-27 17:22:15 +0300

    feat: add user controller

---
+++ src/main/java/com/squad/squad/controller/UserController.java
@@ -0,0 +1,77 @@
+package com.squad.squad.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.squad.squad.dto.ResetPasswordRequest; // Add this import statement
+
+import com.squad.squad.dto.UserDTO;
+import com.squad.squad.entity.User;
+import com.squad.squad.service.UserService;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+
+        if (userService.existsByUsername(user.getUsername())) {
+            return ResponseEntity.badRequest().body("Username is already taken");
+        }
+
+        userService.registerUser(user);
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    @DeleteMapping("/{username}")
+    public ResponseEntity<String> deleteUser(@PathVariable String username) {
+
+        if (!userService.existsByUsername(username)) {
+            return ResponseEntity.badRequest().body("user not found");
+        }
+
+        userService.deleteByUsername(username);
+
+        return ResponseEntity.ok("User deleted");
+    }
+
+    @PutMapping("/{username}")
+    public ResponseEntity<String> updateUserByUsername(@PathVariable String username, @RequestBody User updatedUser) {
+        try {
+            userService.updateUser(username, updatedUser);
+            return ResponseEntity.ok("User updated successfully");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    // ADMIN yetkisine sahip olanlar tarafından erişilebilecek şifre sıfırlama
+    // endpoint'i
+    @PostMapping("/admin/reset-password")
+    public ResponseEntity<String> resetPassword(@RequestBody ResetPasswordRequest request) {
+        String result = userService.resetPassword(request.getUsername(), request.getNewPassword());
+        return ResponseEntity.ok(result);
+    }
+
+}
