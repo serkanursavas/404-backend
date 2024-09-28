4e77ca4082d9d3fe38b12bbf29e13bd79e4ff4f3
serkanursavas <serkan.ursavas@icloud.com>
2024-09-29 00:35:47 +0300

    feat: add custom filter

--- src/main/java/com/squad/squad/repository/RosterRepository.java
+++ src/main/java/com/squad/squad/repository/RosterRepository.java
@@ -1,9 +1,12 @@
 package com.squad.squad.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.squad.squad.entity.Roster;
 
 public interface RosterRepository extends JpaRepository<Roster, Integer> {
 
+    List<Roster> findRosterByGameId(Integer gameId);
 }
