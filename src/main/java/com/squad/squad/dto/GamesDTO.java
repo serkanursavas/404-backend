80a73796f32493748e37497128942982ed6418ee
serkanursavas <serkan.ursavas@icloud.com>
2024-09-28 17:53:28 +0300

    feat: adding games DTO

---
+++ src/main/java/com/squad/squad/dto/GamesDTO.java
@@ -0,0 +1,59 @@
+package com.squad.squad.dto;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class GamesDTO {
+
+    private Integer id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime dateTime;
+
+    private Integer homeTeamScore = 0;
+    private Integer awayTeamScore = 0;
+
+    public GamesDTO() {
+    }
+
+    public GamesDTO(Integer id, LocalDateTime dateTime, Integer homeTeamScore,
+            Integer awayTeamScore) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.homeTeamScore = homeTeamScore;
+        this.awayTeamScore = awayTeamScore;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Integer getHomeTeamScore() {
+        return homeTeamScore;
+    }
+
+    public void setHomeTeamScore(Integer homeTeamScore) {
+        this.homeTeamScore = homeTeamScore;
+    }
+
+    public Integer getAwayTeamScore() {
+        return awayTeamScore;
+    }
+
+    public void setAwayTeamScore(Integer awayTeamScore) {
+        this.awayTeamScore = awayTeamScore;
+    }
+
+}
