bcadbd11c783f77698e097e5449df6ed10c9df95
serkanursavas <serkan.ursavas@icloud.com>
2024-09-28 03:30:21 +0300

    feat: add active player filter

--- src/main/java/com/squad/squad/repository/PlayerRepository.java
+++ src/main/java/com/squad/squad/repository/PlayerRepository.java
@@ -1,9 +1,13 @@
 package com.squad.squad.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.squad.squad.entity.Player;
 
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
+
+    List<Player> findByActive(boolean active);
 
 }
