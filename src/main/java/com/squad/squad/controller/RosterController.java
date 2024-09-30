9b39f3a0bb03101f422b8e024027e06e7e53ff12
serkanursavas <serkan.ursavas@icloud.com>
2024-09-30 23:45:53 +0300

    feat: add update roster endpoint

--- src/main/java/com/squad/squad/controller/RosterController.java
+++ src/main/java/com/squad/squad/controller/RosterController.java
@@ -3,12 +3,16 @@ package com.squad.squad.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.squad.squad.dto.RosterDTO;
 import com.squad.squad.entity.Roster;
 import com.squad.squad.service.RosterService;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/rosters")
@@ -18,6 +22,7 @@ public class RosterController {
 
     public RosterController(RosterService rosterService) {
         this.rosterService = rosterService;
+
     }
 
     @GetMapping("")
@@ -25,4 +30,14 @@ public class RosterController {
         return rosterService.getAllRosters();
     }
 
+    @PutMapping("")
+    public ResponseEntity<String> updateRosters(@RequestBody List<RosterDTO> rosters) {
+
+        for (RosterDTO roster : rosters) {
+            rosterService.updateRoster(roster);
+        }
+
+        return ResponseEntity.ok("success");
+
+    }
 }
