2e77bebfcc9e367c0b3f80932f02efde41be5788
serkanursavas <serkan.ursavas@icloud.com>
2024-09-30 23:50:17 +0300

    feat: adding update roster and update roster rating

--- src/main/java/com/squad/squad/service/RosterService.java
+++ src/main/java/com/squad/squad/service/RosterService.java
@@ -13,13 +13,22 @@ import com.squad.squad.repository.RosterRepository;
 public class RosterService {
 
     private final RosterRepository rosterRepository;
+    private final RatingService ratingService;
 
-    public RosterService(RosterRepository rosterRepository) {
+    public RosterService(RosterRepository rosterRepository, RatingService ratingService) {
         this.rosterRepository = rosterRepository;
+        this.ratingService = ratingService;
     }
 
     public List<Roster> getAllRosters() {
         return rosterRepository.findAll();
+    }
+
+    public Roster getRosterById(Integer id) {
+
+        return rosterRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Roster not found with id: " + id));
+
     }
 
     public List<RosterDTO> findRosterByGameId(Integer gameId) {
@@ -29,6 +38,7 @@ public class RosterService {
         List<RosterDTO> rosterDTOs = rosters.stream()
                 .map(roster -> {
                     RosterDTO dto = new RosterDTO();
+                    dto.setId(roster.getId());
                     dto.setTeamColor(roster.getTeamColor());
                     dto.setPlayerId(roster.getPlayer().getId());
                     dto.setRating(roster.getRating());
@@ -42,6 +52,31 @@ public class RosterService {
 
     public Roster saveRoster(Roster roster) {
         return rosterRepository.save(roster);
+    }
+
+    public Roster updateRoster(RosterDTO updatedRoster) {
+
+        Roster existingRoster = rosterRepository.findById(updatedRoster.getId())
+                .orElseThrow(() -> new RuntimeException("Roster not found with id: " + updatedRoster.getId()));
+
+        existingRoster.setTeamColor(updatedRoster.getTeamColor());
+
+        return rosterRepository.save(existingRoster);
+    }
+
+    public void updateRatingsForGame(Integer gameId) {
+
+        List<Roster> rosters = rosterRepository.findRosterByGameId(gameId);
+
+        for (Roster roster : rosters) {
+            double newRating = ratingService.calculateAvarageRating(roster);
+            roster.setRating(newRating);
+            rosterRepository.save(roster);
+        }
+    }
+
+    public void deleteByGameId(Integer id) {
+        rosterRepository.deleteByGameId(id);
     }
 
 }
