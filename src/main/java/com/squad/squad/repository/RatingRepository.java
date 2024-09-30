b10d1ce1022d9171023765077f78821869217c2c
serkanursavas <serkan.ursavas@icloud.com>
2024-09-30 23:48:15 +0300

    reafactor: add custom queries

--- src/main/java/com/squad/squad/repository/RatingRepository.java
+++ src/main/java/com/squad/squad/repository/RatingRepository.java
@@ -1,9 +1,18 @@
 package com.squad.squad.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.squad.squad.entity.Rating;
+import com.squad.squad.entity.Roster;
+
+import java.util.List;
 
 public interface RatingRepository extends JpaRepository<Rating, Integer> {
 
+    List<Rating> findByRoster(Roster roster);
+
+    @Query("SELECT COUNT(r) FROM Rating r WHERE r.roster.game.id = :game_id")
+    Integer countByRosterGameId(@Param("game_id") Integer game_id);
 }
