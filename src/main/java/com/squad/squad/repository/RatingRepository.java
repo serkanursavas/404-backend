981176222f15345e57f06371174b89c69064afb3
serkanursavas <serkan.ursavas@icloud.com>
2024-10-02 22:02:49 +0300

    refactor: general rating optimization

--- src/main/java/com/squad/squad/repository/RatingRepository.java
+++ src/main/java/com/squad/squad/repository/RatingRepository.java
@@ -16,4 +16,5 @@ public interface RatingRepository extends JpaRepository<Rating, Integer> {
     @Query("SELECT COUNT(r) FROM Rating r WHERE r.roster.game.id = :game_id AND r.roster.teamColor = :team_color")
     Integer countByRosterGameIdAndTeamColor(@Param("game_id") Integer game_id, @Param("team_color") String team_color);
 
+    Double findAverageRatingByRoster(Roster roster);
 }
