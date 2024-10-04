c27f1e0d7e01c18f16f86da8e3ce48bb6c5db745
serkanursavas <serkan.ursavas@icloud.com>
2024-10-04 12:22:59 +0300

    refactor: player splitting to impl

--- src/main/java/com/squad/squad/service/PlayerService.java
+++ src/main/java/com/squad/squad/service/PlayerService.java
@@ -2,52 +2,18 @@ package com.squad.squad.service;
 
 import java.util.List;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import com.squad.squad.entity.Player;
-import com.squad.squad.exception.PlayerNotFoundException;
-import com.squad.squad.repository.PlayerRepository;
-
-import jakarta.transaction.Transactional;
 
 @Service
-public class PlayerService {
+public interface PlayerService {
 
-    private final PlayerRepository playerRepository;
+    List<Player> getAllActivePlayers();
 
-    public PlayerService(PlayerRepository playerRepository) {
-        this.playerRepository = playerRepository;
+    Player getPlayerById(Integer id);
 
-    }
+    Player updatePlayer(Player player);
 
-    public List<Player> getAllActivePlayers() {
-        return playerRepository.findByActive(true);
-    }
-
-    public Player getPlayerById(Integer id) {
-        return playerRepository.findByIdAndActive(id, true)
-                .orElseThrow(() -> new PlayerNotFoundException("Player not found with id: " + id));
-    }
-
-    @Transactional
-    public Player updatePlayer(Player updatedPlayer) {
-        Player existingPlayer = playerRepository.findById(updatedPlayer.getId())
-                .orElseThrow(() -> new RuntimeException("Player not found with id: " + updatedPlayer.getId()));
-
-        BeanUtils.copyProperties(updatedPlayer, existingPlayer);
-        existingPlayer.setActive(true);
-
-        return playerRepository.save(existingPlayer);
-
-    }
-
-    @Transactional
-    public void deletePlayerById(Integer id) {
-        Player player = playerRepository.findById(id)
-                .orElseThrow(() -> new PlayerNotFoundException("Player not found with id: " + id));
-
-        playerRepository.delete(player);
-    }
-
+    void deletePlayerById(Integer id);
 }
---
+++ src/main/java/com/squad/squad/service/impl/PlayerServiceImpl.java
@@ -0,0 +1,54 @@
+package com.squad.squad.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+
+import com.squad.squad.entity.Player;
+import com.squad.squad.exception.PlayerNotFoundException;
+import com.squad.squad.repository.PlayerRepository;
+import com.squad.squad.service.PlayerService;
+
+import jakarta.transaction.Transactional;
+
+public class PlayerServiceImpl implements PlayerService {
+    private final PlayerRepository playerRepository;
+
+    public PlayerServiceImpl(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    @Override
+    public List<Player> getAllActivePlayers() {
+        return playerRepository.findByActive(true);
+    }
+
+    @Override
+    public Player getPlayerById(Integer id) {
+        return playerRepository.findByIdAndActive(id, true)
+                .orElseThrow(() -> new PlayerNotFoundException("Player not found with id: " + id));
+    }
+
+    @Override
+    @Transactional
+    public Player updatePlayer(Player updatedPlayer) {
+        Player existingPlayer = playerRepository.findById(updatedPlayer.getId())
+                .orElseThrow(() -> new RuntimeException("Player not found with id: " + updatedPlayer.getId()));
+
+        BeanUtils.copyProperties(updatedPlayer, existingPlayer);
+        existingPlayer.setActive(true);
+
+        return playerRepository.save(existingPlayer);
+
+    }
+
+    @Override
+    @Transactional
+    public void deletePlayerById(Integer id) {
+        Player player = playerRepository.findById(id)
+                .orElseThrow(() -> new PlayerNotFoundException("Player not found with id: " + id));
+
+        playerRepository.delete(player);
+    }
+
+}
