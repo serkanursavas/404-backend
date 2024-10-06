6f7202ab1593e3678bc89f9f469115c122857af1
serkanursavas <serkan.ursavas@icloud.com>
2024-10-06 20:20:27 +0300

    fix: adding repo anotation

--- src/main/java/com/squad/squad/repository/PlayerRepository.java
+++ src/main/java/com/squad/squad/repository/PlayerRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.squad.squad.entity.Player;
 
+@Repository
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
 
     List<Player> findByActive(boolean active);
--- src/main/java/com/squad/squad/repository/RosterRepository.java
+++ src/main/java/com/squad/squad/repository/RosterRepository.java
@@ -3,9 +3,11 @@ package com.squad.squad.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.squad.squad.entity.Roster;
 
+@Repository
 public interface RosterRepository extends JpaRepository<Roster, Integer> {
 
     List<Roster> findRosterByGameId(Integer gameId);
