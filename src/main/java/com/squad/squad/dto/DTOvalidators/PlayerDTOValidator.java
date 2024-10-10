640ed883983790a012d50b519d40ee00343bcf50
serkanursavas <serkan.ursavas@icloud.com>
2024-10-10 16:05:50 +0300

    feat: modife playerdto and add updateplayer dto for dependecies to user

--- src/main/java/com/squad/squad/dto/DTOvalidators/PlayerDTOValidator.java
+++ src/main/java/com/squad/squad/dto/DTOvalidators/PlayerDTOValidator.java
@@ -3,6 +3,7 @@ package com.squad.squad.dto.DTOvalidators;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.squad.squad.dto.PlayerCreateDTO;
 import org.springframework.stereotype.Component;
 
 import com.squad.squad.dto.PlayerDTO;
@@ -10,7 +11,7 @@ import com.squad.squad.dto.PlayerDTO;
 @Component
 public class PlayerDTOValidator {
 
-    public List<String> validate(PlayerDTO player) {
+    public List<String> validate(PlayerCreateDTO player) {
 
         List<String> errors = new ArrayList<>();
 
@@ -28,6 +29,32 @@ public class PlayerDTOValidator {
 
         if (player.getPosition() == null || player.getPosition().isEmpty()) {
             errors.add("Player position info cannot bi empty");
+        }
+
+        return errors;
+    }
+
+    public List<String> validateUpdate(PlayerDTO player) {
+        List<String> errors = new ArrayList<>();
+
+        // Eğer isim boşsa veya null ise hata ekle
+        if (player.getName() != null && player.getName().isEmpty()) {
+            errors.add("Player name cannot be empty if provided.");
+        }
+
+        // Soyadı kontrol et
+        if (player.getSurname() != null && player.getSurname().isEmpty()) {
+            errors.add("Player surname cannot be empty if provided.");
+        }
+
+        // Ayak bilgisi kontrol et
+        if (player.getFoot() != null && player.getFoot().isEmpty()) {
+            errors.add("Player foot info cannot be empty if provided.");
+        }
+
+        // Pozisyon bilgisi kontrol et
+        if (player.getPosition() != null && player.getPosition().isEmpty()) {
+            errors.add("Player position info cannot be empty if provided.");
         }
 
         return errors;
