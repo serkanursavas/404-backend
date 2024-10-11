f2ae548ad9dbf68ffea70eaee40c21f10c6fa02b
serkanursavas <serkan.ursavas@icloud.com>
2024-10-11 13:28:34 +0300

    feat: add player dtos for req

---
+++ src/main/java/com/squad/squad/dto/player/GetAllActivePlayersDTO.java
@@ -0,0 +1,22 @@
+package com.squad.squad.dto.player;
+
+public class GetAllActivePlayersDTO {
+    private Integer id;
+    private String playerName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+}
