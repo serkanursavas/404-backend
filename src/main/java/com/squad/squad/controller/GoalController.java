237d1d32ec2d4af07106744d8ede860cedbfca6c
serkanursavas <serkan.ursavas@icloud.com>
2024-10-12 02:09:13 +0300

    feat: adding extra validation for addGoal

--- src/main/java/com/squad/squad/controller/GoalController.java
+++ src/main/java/com/squad/squad/controller/GoalController.java
@@ -1,15 +1,16 @@
 package com.squad.squad.controller;
 
 import com.squad.squad.dto.DTOvalidators.GoalDTOValidator;
+import com.squad.squad.dto.goal.AddGoalsRequestDTO;
 import com.squad.squad.dto.goal.GoalAddRequestDTO;
+import com.squad.squad.entity.Roster;
+import com.squad.squad.service.GameService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.squad.squad.dto.GoalDTO;
 import com.squad.squad.service.GoalService;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -21,21 +22,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class GoalController {
 
     private final GoalService goalService;
+    private final GameService gameService;
     private final GoalDTOValidator goalDTOValidator;
 
-    public GoalController(GoalService goalService, GoalDTOValidator goalDTOValidator) {
+    public GoalController(GoalService goalService, GameService gameService, GoalDTOValidator goalDTOValidator) {
         this.goalService = goalService;
+        this.gameService = gameService;
         this.goalDTOValidator = goalDTOValidator;
     }
 
     @PostMapping("/admin/addGoals")
-    public ResponseEntity<?> addGoals(@RequestBody List<GoalAddRequestDTO> goalDtos) {
-        List<String> errors = goalDTOValidator.validate(goalDtos);
+    public ResponseEntity<?> addGoals(@RequestBody AddGoalsRequestDTO requestDto) {
+
+        Integer gameId = requestDto.getGameId();
+        List<GoalAddRequestDTO> goalDtos = requestDto.getGoals();
+
+        if (goalDtos == null || goalDtos.isEmpty()) {
+            return ResponseEntity.badRequest().body("Goal list cannot be empty");
+        }
+
+        List<Roster> gameRosters = gameService.getRostersByGameId(gameId);
+        
+        List<String> errors = goalDTOValidator.validate(goalDtos, gameRosters);
         if (!errors.isEmpty()) {
             return ResponseEntity.badRequest().body(errors);
         }
 
-        goalService.addGoals(goalDtos);
+        goalService.addGoals(requestDto);
         return ResponseEntity.ok("Goals created successfully");
     }
 }
