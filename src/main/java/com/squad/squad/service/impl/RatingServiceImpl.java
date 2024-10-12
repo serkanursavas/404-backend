9e171652fda2c5d85fc63a0c2c2b680d847c6d45
serkanursavas <serkan.ursavas@icloud.com>
2024-10-12 03:43:45 +0300

    refactor: add validation logic for save rating

--- src/main/java/com/squad/squad/service/impl/RatingServiceImpl.java
+++ src/main/java/com/squad/squad/service/impl/RatingServiceImpl.java
@@ -2,9 +2,13 @@ package com.squad.squad.service.impl;
 
 import java.util.List;
 
+import com.squad.squad.dto.rating.AddRatingRequestDTO;
 import com.squad.squad.entity.Game;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import com.squad.squad.dto.GameDTO;
@@ -34,7 +38,6 @@ public class RatingServiceImpl implements RatingService {
                              RosterService rosterService, GameService gameService) {
         this.ratingRepository = ratingRepository;
         this.playerService = playerService;
-
         this.gameService = gameService;
     }
 
@@ -44,20 +47,49 @@ public class RatingServiceImpl implements RatingService {
     }
 
     @Override
-    @Transactional
-    public void saveRating(List<RatingDTO> ratings) {
-
+    public void saveRating(List<AddRatingRequestDTO> ratings) {
         Integer gameId = null;
-        String teamColor = null;
+        String voterTeamColor = null;
 
-        for (RatingDTO ratingDto : ratings) {
+        if (ratings == null || ratings.isEmpty()) {
+            throw new IllegalArgumentException("Ratings list cannot be null or empty");
+        }
+
+        for (AddRatingRequestDTO ratingDto : ratings) {
             Player existingPlayer = playerMapper.playerDTOToPlayer(
                     playerService.getPlayerById(ratingDto.getPlayerId()));
-            Roster existingRoster = rosterService.getRosterById(ratingDto.getPlayerId());
 
-            if (gameId == null && teamColor == null) {
+            Roster existingRoster = rosterService.getRosterById(ratingDto.getRosterId());
+
+            if (gameId == null && voterTeamColor == null) {
                 gameId = existingRoster.getGame().getId();
-                teamColor = existingRoster.getTeamColor();
+
+                Roster voterRoster = rosterService.getRosterByPlayerId(existingPlayer.getId());
+
+                if (voterRoster == null) {
+                    throw new IllegalStateException("Voter player is not in the roster for this game." + getCurrentPlayerId());
+                }
+
+                voterTeamColor = voterRoster.getTeamColor();
+
+                Game existingGame = gameService.findGameById(gameId);
+                if (!existingGame.isPlayed() || !existingGame.isVoteMode() || existingGame.isVoted()) {
+                    throw new IllegalStateException("Voting is not allowed for this game. Check game state.");
+                }
+            }
+
+            if (ratingDto.getPlayerId().equals(existingRoster.getPlayer().getId())) {
+                throw new IllegalArgumentException("Players cannot vote for themselves.");
+            }
+
+            if (!voterTeamColor.equalsIgnoreCase(existingRoster.getTeamColor())) {
+                throw new IllegalArgumentException("Players can only vote for their teammates.");
+            }
+
+            boolean hasAlreadyVoted = ratingRepository.existsByPlayerIdAndRosterId(existingPlayer.getId(), ratingDto.getRosterId());
+            System.out.println(existingPlayer.getId() + " " + ratingDto.getRosterId());
+            if (hasAlreadyVoted) {
+                throw new IllegalArgumentException("You have already voted for this player.");
             }
 
             Rating rating = new Rating();
@@ -68,7 +100,7 @@ public class RatingServiceImpl implements RatingService {
             ratingRepository.save(rating);
         }
 
-        checkIfVotingIsComplete(gameId, teamColor);
+        checkIfVotingIsComplete(gameId, voterTeamColor);
     }
 
     @Override
@@ -109,5 +141,13 @@ public class RatingServiceImpl implements RatingService {
     @Transactional
     public void clearAllRatings() {
         ratingRepository.deleteAll();
+    }
+
+    public Integer getCurrentPlayerId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails userDetails) {
+            return Integer.valueOf(userDetails.getUsername());
+        }
+        throw new IllegalStateException("Current user not found");
     }
 }
