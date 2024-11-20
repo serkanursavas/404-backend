186447765ee0d38c00330a3f98d3f19cf8c57eeb
serkanursavas <serkan.ursavas@icloud.com>
2024-11-20 21:44:58 +0300

    refactor: top rating list query

--- src/main/java/com/squad/squad/repository/PlayerRepository.java
+++ src/main/java/com/squad/squad/repository/PlayerRepository.java
@@ -17,11 +17,24 @@ public interface PlayerRepository extends JpaRepository<Player, Integer> {
 
     Optional<Player> findByIdAndActive(Integer id, boolean active);
 
-    @Query(value = "SELECT p.id as playerId, p.name, p.surname, p.rating " +
+    @Query(value = "WITH RecentGames AS ( " +
+            "    SELECT g.id " +
+            "    FROM game g " +
+            "    WHERE g.is_played = 1 " +
+            "    ORDER BY g.date_time DESC " +
+            "    LIMIT 2 " +
+            ") " +
+            "SELECT p.id AS playerId, p.name, p.surname, p.rating " +
             "FROM player p " +
             "WHERE p.rating IS NOT NULL " +
+            "  AND p.id IN ( " +
+            "      SELECT DISTINCT r.player_id " +
+            "      FROM roster r " +
+            "      WHERE r.game_id IN (SELECT id FROM RecentGames) " +
+            "  ) " +
             "ORDER BY p.rating DESC " +
-            "LIMIT 5", nativeQuery = true)
+            "LIMIT 5",
+            nativeQuery = true)
     List<Object[]> findTopRatedPlayers();
 
     List<Player> findByIdIn(List<Integer> ids);
