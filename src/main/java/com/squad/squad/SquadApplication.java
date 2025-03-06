714f714d5d5e5650e69555ffb6d3aaf1d3f08b2a
serkanursavas <serkan.ursavas@icloud.com>
2025-03-06 21:30:25 +0300

    fix: delete unneccesary scheduler

--- src/main/java/com/squad/squad/SquadApplication.java
+++ src/main/java/com/squad/squad/SquadApplication.java
@@ -3,10 +3,8 @@ package com.squad.squad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableCaching
 public class SquadApplication {
 
