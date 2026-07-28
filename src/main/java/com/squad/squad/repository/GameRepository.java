fbc8061bda693079085748fc1e805b70f4b592c8
serkanursavas <serkan.ursavas@icloud.com>
2026-07-29 02:14:33 +0300

    Remove unused legacy findMvpPlayers query
    Broke on the MvpDTO constructor change and was never called anywhere.

--- src/main/java/com/squad/squad/repository/GameRepository.java
+++ src/main/java/com/squad/squad/repository/GameRepository.java
@@ -1,6 +1,5 @@
 package com.squad.squad.repository;
 
-import com.squad.squad.dto.MvpDTO;
 import com.squad.squad.entity.Game;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -73,14 +72,4 @@ public interface GameRepository extends JpaRepository<Game, Integer> {
     Game findByIsPlayedFalse();
     boolean existsByIsPlayedFalseOrIsVotedFalse();
     Page<Game> findAllByOrderByDateTimeDesc(Pageable pageable);
-
-    @Query("SELECT new com.squad.squad.dto.MvpDTO(p.id, p.name, p.surname, p.photo, p.position, r.rating) " +
-            "FROM Player p " +
-            "JOIN Roster r ON p.id = r.player.id " +
-            "JOIN Game g ON r.game.id = g.id " +
-            "WHERE g.isPlayed = true AND g.isVoted = true AND g.dateTime = (" +
-            "SELECT MAX(g2.dateTime) FROM Game g2 WHERE g2.isPlayed = true AND g2.isVoted = true" +
-            ") " +
-            "ORDER BY r.rating DESC")
-    List<MvpDTO> findMvpPlayers();
 }
